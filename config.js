3591f1f84face8fb32306cb3ac58a089c69a357c
Krister <krister@Kristers-MacBook-Pro.local>
2023-01-30 20:26:29 +0200

    add config.js

---
+++ config.js
@@ -0,0 +1,52 @@
+const config = { 
+  "courseName": "Programmeerimine I",
+  "courseCode": "HKI5097.HK",
+  "slug": "Programmeerimine_I",
+  "courseUrl": "https://ois2.tlu.ee/tluois/aine/HKI5097.HK",
+  "active": true,
+  "docs": [
+    {
+      "slug": "about",
+      "name": "Ainekursusest"
+    }
+  ],
+  "additionalMaterials": [
+    {
+      "slug": "lisamaterjalid",
+      "name": "Aine lisamaterjalid"
+    }
+  ],
+  "lessons": [
+    {
+      "name": "Loeng 1 - Sissejuhatus",
+      "slug": "loeng_01",
+      "components": ["nodejs", "NpM"],
+      "additionalMaterials": [
+        {
+          "slug": "lisamaterjalid",
+          "name": "Loengu lisamaterjalid"
+        }
+      ]
+    }
+  ],
+  "concepts": [
+    {
+      "slug": "nodejs",
+      "name": "NodeJS"
+    },
+    {
+      "slug": "nPm",
+      "name": "NPM"
+    }
+  ],
+  "practices": [
+    {
+      "slug": "praktikum_01",
+      "name": "Näidis Praktikum"
+    },
+    {
+      "slug": "praktikum_02",
+      "name": "Praktikum 2"
+    }
+  ]
+}
