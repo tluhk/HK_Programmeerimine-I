e404be0182c11ca005674738bfb67bba23212af4
seppkh <62253084+seppkh@users.noreply.github.com>
2023-04-20 13:21:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,52 +1,92 @@
-const config = { 
-  "courseName": "Programmeerimine I",
-  "courseCode": "HKI5097.HK",
-  "slug": "Programmeerimine_I",
-  "courseUrl": "https://ois2.tlu.ee/tluois/aine/HKI5097.HK",
-  "active": true,
-  "docs": [
+{
+  courseUrl: "https://ois2.tlu.ee/tluois/aine/HKI5097.HK",
+  teacherUsername: "priit",
+  active: true,
+  semester: "K2022",
+  docs: [
     {
-      "slug": "about",
-      "name": "Ainekursusest"
+      slug: "about",
+      name: "Aine info"
     }
   ],
-  "additionalMaterials": [
+  additionalMaterials: [
     {
-      "slug": "lisamaterjalid",
-      "name": "Aine lisamaterjalid"
+      slug: "lisamaterjalid",
+      name: "Aine lisamaterjalid"
     }
   ],
-  "lessons": [
+  lessons: [
     {
-      "name": "Loeng 1 - Sissejuhatus",
-      "slug": "loeng_01",
-      "components": ["nodejs", "NpM"],
-      "additionalMaterials": [
+      slug: "loeng_01",
+      name: "Loeng 1 - Sissejuhatus",
+      uuid: "523dd0ed-e2fe-453c-95d0-e748cf483c42",
+      components: ["nodejs", "npm"],
+      additionalMaterials: [
         {
-          "slug": "lisamaterjalid",
-          "name": "Loengu lisamaterjalid"
+          slug: "lisamaterjalid",
+          name: "Loengu lisamaterjalid"
+        }
+      ]
+    },
+    {
+      slug: "loeng_02",
+      name: "Loeng 2 - Praktikumid",
+      uuid: "fd818e1d-7117-46b9-91be-dc50671a6c3e",
+      components: ["praktikum_01", "praktikum_02"],
+      additionalMaterials: [
+        {
+          slug: "lisamaterjalid",
+          name: "Loengu lisamaterjalid"
+        }
+      ]
+    },
+    {
+      slug: "loeng_03",
+      name: "Loeng 3 - Uuesti sissejuhatus",
+      uuid: "40329d0b-a80b-4b64-9fc5-de5103cba505",
+      components: ["nodejs", "npm"],
+      additionalMaterials: [
+        {
+          slug: "lisamaterjalid",
+          name: "Loengu lisamaterjalid"
+        }
+      ]
+    },
+    {
+      slug: "loeng_04",
+      name: "Loeng 4 - Uuesti praktikumid",
+      uuid: "88f82410-758d-476d-9a58-6016b39ac9d0",
+      components: ["praktikum_01", "praktikum_02"],
+      additionalMaterials: [
+        {
+          slug: "lisamaterjalid",
+          name: "Loengu lisamaterjalid"
         }
       ]
     }
   ],
-  "concepts": [
+  concepts: [
     {
-      "slug": "nodejs",
-      "name": "NodeJS"
+      slug: "nodejs",
+      name: "NodeJS",
+      uuid: "fbbbf667-ec8b-4287-baad-6975b917f505"
     },
     {
-      "slug": "nPm",
-      "name": "NPM"
+      slug: "npm",
+      name: "NPM",
+      uuid: "ea8b329e-1585-4d13-abcb-60d2c01a4da3"
     }
   ],
-  "practices": [
+  practices: [
     {
-      "slug": "praktikum_01",
-      "name": "Näidis Praktikum"
+      slug: "praktikum_01",
+      name: "Praktikum 1",
+      uuid: "9e552ecd-728c-4556-91e9-d42611393dbe"
     },
     {
-      "slug": "praktikum_02",
-      "name": "Praktikum 2"
+      slug: "praktikum_02",
+      name: "Praktikum 2",
+      uuid: "750a3a40-6f2e-4575-b684-79608403642c"
     }
   ]
 }
